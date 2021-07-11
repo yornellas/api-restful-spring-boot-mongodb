b2b6d226cbff49127479818c00405354498d8df2
Yolanda Ornellas <ymf.ornellas@icloud.com>
2021-07-11 14:43:23 -0300

    User POST insert

--- src/main/java/com/github/yornellas/apispringbootmongodb/services/UserService.java
+++ src/main/java/com/github/yornellas/apispringbootmongodb/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.github.yornellas.apispringbootmongodb.domain.User;
+import com.github.yornellas.apispringbootmongodb.dto.UserDTO;
 import com.github.yornellas.apispringbootmongodb.repositories.UserRepository;
 import com.github.yornellas.apispringbootmongodb.services.exceptions.ObjectNotFoundException;
 
@@ -24,4 +25,13 @@ public class UserService {
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
+	
+	public User insert(User obj) {
+		return repository.insert(obj);
+	}
+	
+	public User fromDto(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
+	
 }
