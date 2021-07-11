11cf0c40d37975db318a53df09941fd9bcb1c459
Yolanda Ornellas <ymf.ornellas@icloud.com>
2021-07-11 12:14:12 -0300

    First commit - create project

---
+++ src/main/java/com/github/yornellas/apispringbootmongodb/ApiSpringBootMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.github.yornellas.apispringbootmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSpringBootMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSpringBootMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/yornellas/apispringbootmongodb/ApiSpringBootMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.yornellas.apispringbootmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSpringBootMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
