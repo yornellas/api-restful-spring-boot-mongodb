f5f39e4f435247ab8c5f252f833ba93b24d96a8b
Yolanda Ornellas <ymf.ornellas@icloud.com>
2021-07-11 13:23:24 -0300

    Create UserDTO

--- src/main/java/com/github/yornellas/apispringbootmongodb/resources/UserResource.java
+++ src/main/java/com/github/yornellas/apispringbootmongodb/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.github.yornellas.apispringbootmongodb.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.github.yornellas.apispringbootmongodb.domain.User;
+import com.github.yornellas.apispringbootmongodb.dto.UserDTO;
 import com.github.yornellas.apispringbootmongodb.services.UserService;
 
 @RestController
@@ -19,8 +21,9 @@ public class UserResource {
 	public UserService service;
 	
 	@GetMapping
-	public ResponseEntity<List<User>> findAll() {
+	public ResponseEntity<List<UserDTO>> findAll() {
 		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list);	
+		List<UserDTO> listDto = list.stream().map(e -> new UserDTO(e)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);	
 	}
 }
