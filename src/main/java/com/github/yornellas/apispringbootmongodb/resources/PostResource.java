720daab4a1e4d0e63b9005acfe00fd50fc2d63dd
Yolanda Ornellas <ymf.ornellas@icloud.com>
2021-08-01 12:28:21 -0300

    simple consult with query methods

--- src/main/java/com/github/yornellas/apispringbootmongodb/resources/PostResource.java
+++ src/main/java/com/github/yornellas/apispringbootmongodb/resources/PostResource.java
@@ -1,5 +1,6 @@
 package com.github.yornellas.apispringbootmongodb.resources;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,9 +8,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.github.yornellas.apispringbootmongodb.domain.Post;
+import com.github.yornellas.apispringbootmongodb.resources.util.URL;
 import com.github.yornellas.apispringbootmongodb.services.PostService;
 
 @RestController
@@ -25,4 +28,10 @@ public class PostResource {
 		return ResponseEntity.ok().body(obj);
 	}
 	
+	@GetMapping(value="/titlesearch")
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="") String text) {
+		text = URL.decodeParam(text);
+		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
+	}
 }
--- src/main/java/com/github/yornellas/apispringbootmongodb/services/PostService.java
+++ src/main/java/com/github/yornellas/apispringbootmongodb/services/PostService.java
@@ -1,5 +1,6 @@
 package com.github.yornellas.apispringbootmongodb.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,5 +22,9 @@ public class PostService {
 			throw new ObjectNotFoundException("Post não encontrado");
 		}
 		return obj;
+	}
+	
+	public List<Post> findByTitle(String text) {
+		return repository.findByTitleContainingIgnoreCase(text);
 	}
 }
