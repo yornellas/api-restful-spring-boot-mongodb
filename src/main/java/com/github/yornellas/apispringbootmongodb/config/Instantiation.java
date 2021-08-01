c7964575cd0e54216b9b81e58c49e91095ff4b9f
Yolanda Ornellas <ymf.ornellas@icloud.com>
2021-08-01 10:44:08 -0300

    References User posts

--- src/main/java/com/github/yornellas/apispringbootmongodb/config/Instantiation.java
+++ src/main/java/com/github/yornellas/apispringbootmongodb/config/Instantiation.java
@@ -42,6 +42,9 @@ public class Instantiation implements CommandLineRunner{
 		Post post2 = new Post(null, sdf.parse("21/03/2019"), "Bom dia!", "Acordei feliz hoje!", new AuthorDTO(nayara));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
+		
+		nayara.getPosts().addAll(Arrays.asList(post1, post2));
+		userRepository.save(nayara);
 	}
 
 }
